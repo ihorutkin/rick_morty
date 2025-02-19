456b41c49b8884eec62ac1ab31b258b4f5f0d43b
Ihor Utkin <igorutkin2002@gmail.com>
2025-02-19 22:10:27 +0100

    Dockerfile was adde

--- characters/views.py
+++ characters/views.py
@@ -7,7 +7,14 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 
 from characters.models import Character
+from characters.pagination import CharactersListPagination
 from characters.serializers import CharacterSerializer
+
+
+def get_random_character() -> Character:
+    pks = Character.objects.values_list("pk", flat=True)
+    random_pk = random.choice(pks)
+    return Character.objects.get(pk=random_pk)
 
 
 @extend_schema(
@@ -18,19 +25,15 @@ from characters.serializers import CharacterSerializer
 @api_view(["GET"])
 def get_random_character_view(request: Request) -> Response:
     """Get random character from Rick and Morty API"""
-    pks = Character.objects.values_list("pk", flat=True)
-    random_pk = random.choice(pks)
-    random_character = Character.objects.get(pk=random_pk)
-
+    random_character = get_random_character()
     serializer = CharacterSerializer(random_character)
 
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-
 class CharacterListView(generics.ListAPIView):
-    queryset = Character.objects.all()
     serializer_class = CharacterSerializer
+    pagination_class = CharactersListPagination
 
     def get_queryset(self):
         queryset = Character.objects.all()
